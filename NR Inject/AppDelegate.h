3bc891729e57107f27e0b7eeb53e36c6daaac59b
Fabian Buentello <fbuentello@mioty.com>
2015-01-20 04:45:41 -0600

    Initial Commit

---
+++ NR Inject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NR Inject
+//
+//  Created by Fabian Buentello on 1/20/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NR Inject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NR Inject
+//
+//  Created by Fabian Buentello on 1/20/15.
+//  Copyright (c) 2015 Fabian Buentello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
